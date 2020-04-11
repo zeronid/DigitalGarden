b64cc6ad909b2a6631f78e0f74f02bf051f71da5
Zeronid <zeronid@gmail.com>
2020-04-11 06:52:25 +0300

    Updated the splash background and font

--- app/src/main/java/com/example/digitalgarden/activities/SplashActivity.java
+++ app/src/main/java/com/example/digitalgarden/activities/SplashActivity.java
@@ -44,6 +44,11 @@ public class SplashActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
+                try {
+                    Thread.sleep(1500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                     authenticateUser();
                     mFirebaseAuth.addAuthStateListener(mAuthStateListener);
             }
